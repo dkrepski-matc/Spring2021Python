88e29607a608a8c59c269096928fb30d4dee160b
Daniel Krepski <dkrepski@madisoncollege.edu>
2021-01-27 11:05:59 -0600

    READY FOR GRADING - week 2 sample script

---
+++ week2-script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This is my week 2 assignment script
+Author: Daniel Krepski
+Date: 01/27/2021
+"""
+print("Hello, my creator's name is Daniel Krepski.")
+print("One thing learned during this exercise was that triple quotes could be used to add block comments as well as pound signs for individual lines.")
+
