238b3e845c5a06b36a25bb775be857a64b9eb996
Daniel Krepski <dkrepski@madisoncollege.edu>
2021-02-03 11:43:54 -0600

    READY FOR GRADING - Week 3 string script

---
+++ week3_strings.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+#This script is the week 3 assignment
+
+varRed = "Red"
+varGreen = "Green"
+varBlue = "Blue"
+varName = "Timmy"
+varLoot = 10.4516295
+
+#Question 1
+print(f"Hello {varName}")
+#Question 2
+print(f"{varRed}-{varGreen}-{varBlue}")
+#Question 3
+print(f"Is this {varGreen.lower()} or {varBlue.lower()}?")
+#Question 4
+print(f"My name is {varName.upper()}")
+#Question 5
+print(f"[{varRed:+^7s}]")
+#Question 6
+print(f"[{varGreen.lower():=<7s}]")
+#Question 7
+print(f"[{varBlue.lower():*>9s}]")
+#Question 8
+print(f"{varBlue*10}")
+#Question 9
+print(varLoot)
+#Question 10
+print(f"{varLoot:<.1f}")
+#Question 11
+print(f"I have ${varLoot:<.2f}")
+#Question 12
+print(f"[{varRed:$^10s}] [{varGreen:$^10s}] [{varBlue:$^10s}]")
+#Question 13
+print(f"[{varRed[::-1]: ^10s}] [{varGreen: ^10s}] [{varBlue[::-1]: ^10s}]")
+#Question 14
+print(f"First Color: [{varRed}] Second Color: [{varGreen}] Third Color: [{varBlue}]")
